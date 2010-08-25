4ab02b49d211f95ddcf7d9fa38274c2bf712abb7
Roman Smrž <roman.smrz@seznam.cz>
2010-08-25 17:25:41 +0200

    Using TokyoCabinet in Swapper

--- Swapper.hs
+++ Swapper.hs
@@ -17,66 +17,78 @@ import System.IO.Unsafe
 
 import qualified System.IO as IO
 
+import qualified TokyoCabinet as TC
 
-data Swapeable a = Swapeable !FilePath !(MVar ()) !(IORef (Weak a))
-data Swapper f a = Swapper !FilePath !(MVar ()) !(IORef Integer) !(f (Swapeable a))
+
+data Swappable a = Swappable
+        { saKey :: !ByteString
+        , saValue :: !(MVar (Weak a))
+        , saDB :: !TC.Database
+        , saLoader :: !(MVar (IO a))
+        }
+
+data Swapper f a = Swapper
+        { spDB :: !TC.Database
+        , spCounter :: !(MVar Integer)
+        , spContent :: !(f (Swappable a))
+        }
 
 
 f =<<! a = a `pseq` (f =<< a)
 return' x = x `pseq` return x
 
 
-swWeak x path = mkWeakPtr x $ Just $ do
-        IO.putStrLn $ "Ukladani " ++ path
-        BS.writeFile path $ runPut (safePut x)
-        IO.putStrLn $ "Ulozeno " ++ path
+swWeak :: (Serialize a) => ByteString -> a -> TC.Database -> MVar (IO a) -> IO (Weak a)
+swWeak key x db mload =
+        mkWeakPtr x $ Just $ do
+                IO.putStrLn $ "Ukladani " ++ show (fst (deserialize key) :: Integer)
+                TC.put db key (serialize x)
+                IO.putStrLn $ "Ulozeno " ++ show (fst (deserialize key) :: Integer)
+
+                putMVar mload $ do
+                        IO.putStrLn $ "Nacitani " ++ show (fst (deserialize key) :: Integer)
+                        value <- TC.get db key
+                        return . fst . deserialize $ value
 
 
-putSwapeable :: (Serialize a, NFData a) => Swapper f a -> a -> Swapeable a
-putSwapeable (Swapper dir lock counter _) x = deepseq x `pseq` unsafePerformIO $ do
-        takeMVar lock
-        c <- readIORef counter
-        modifyIORef counter (+1)
-        putMVar lock ()
+putSwappable :: (Serialize a, NFData a) => Swapper f a -> a -> Swappable a
+putSwappable (Swapper db counter _) x = deepseq x `pseq` unsafePerformIO $ do
+        c <- takeMVar counter
+        putMVar counter (c+1)
 
-        let path = dir ++ "/" ++ show c
-        IO.putStrLn $ "Vytvareni " ++ path
-        nlock <- newMVar ()
-        return' . Swapeable path nlock =<<
-                newIORef =<<! swWeak x path
+        IO.putStrLn $ "Vytvareni " ++ show c
+        mload <- newEmptyMVar
+        mvalue <- newMVar =<<! swWeak (serialize c) x db mload
+        return' $! Swappable (serialize c) mvalue db mload
 
 
-getSwapeable :: (Serialize a) => Swapeable a -> a
-getSwapeable (Swapeable path lock iwx) = unsafePerformIO $ do
-        takeMVar lock
-        mx <- deRefWeak =<< readIORef iwx
-        x <- case mx of
-                  Just x -> return x
-                  Nothing -> do
-                          IO.putStrLn $ "Nacitani " ++ path
-                          x <- return . runGet safeGet =<< BS.readFile path
-                          wx <- swWeak x path
-                          writeIORef iwx wx
-                          return x
-        putMVar lock ()
-        return x
+getSwappable :: (Serialize a) => Swappable a -> a
+getSwappable (Swappable key mwx db mloader) = unsafePerformIO $ do
+        wx <- takeMVar mwx
+        mx <- deRefWeak wx
+        case mx of Just x -> putMVar mwx wx >> return x
+                   Nothing -> do
+                           loader <- takeMVar mloader
+                           x <- loader
+                           putMVar mwx =<< swWeak key x db mloader
+                           return x
 
 
 
 mkSwapper :: (Serialize a, NFData a, Functor f) => FilePath -> f a -> IO (Swapper f a)
-mkSwapper dir v = do
-        lock <- newMVar ()
-        counter <- newIORef (0 :: Integer)
-        let swapper = Swapper dir lock counter $ fmap (putSwapeable swapper) v
+mkSwapper filename v = do
+        db <- TC.open (filename ++ ".tcb")
+        counter <- newMVar (0 :: Integer)
+        let swapper = Swapper db counter $ fmap (putSwappable swapper) v
          in return swapper
 
 
-adding :: (Serialize a, NFData a) => ((Swapeable a) -> f (Swapeable a) -> f (Swapeable a)) ->
+adding :: (Serialize a, NFData a) => ((Swappable a) -> f (Swappable a) -> f (Swappable a)) ->
         (a -> Swapper f a -> Swapper f a)
-adding f x sw@(Swapper dir lock counter v) =
-        let sx = putSwapeable sw x
-         in sx `pseq` Swapper dir lock counter $ f sx v
+adding f x sw@(Swapper mdb counter v) =
+        let sx = putSwappable sw x
+         in sx `pseq` Swapper mdb counter $ f sx v
 
 
-getting :: (Serialize a) => (f (Swapeable a) -> (Swapeable a)) -> (Swapper f a -> a)
-getting f sw@(Swapper _ _ _ xs) = getSwapeable $ f xs
+--getting :: (Serialize a) => (f (Swappable a) -> (Swappable a)) -> (Swapper f a -> a)
+getting f sw@(Swapper _ _ xs) = getSwappable $ f xs
