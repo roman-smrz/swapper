43616bd3e6a0afe996d5559ebba3feabe892f0dd
Roman Smrž <roman.smrz@seznam.cz>
2010-12-06 21:34:21 +0100

    Implementation documentation of Swapper

--- src/Data/Disk/Swapper.hs
+++ src/Data/Disk/Swapper.hs
@@ -28,6 +28,60 @@
 -- 'getFromSnapshot', only indices and auxiliary data are loaded into the
 -- memory at that time; values are read on demand from their database file.
 
+
+
+-- IMPLEMENTATION
+--
+-- Swapper is implemented using the functor given as its first type argument,
+-- containing a certain "placeholders" (encapsulated in internal Swappable
+-- type) instead of (or possibly along with) actual data. When some value is
+-- added to the Swapper object, it is assigned unique numeric key (each Swapper
+-- contains counter for generating these keys) to be used later for indexing in
+-- database and the Swappable object is created for it. It contains the key and
+-- weak pointer to the data; when those are garbage collected and about to be
+-- freed, they are written to the database file of the containing Swapper.
+--
+-- Because loading and deserializing values form database even if such value is
+-- frequently used, is undesirable, a cache mechanism is provided using the
+-- ClockCache data type. (Originally, it was meant to accept any type belonging
+-- to the Cache class, but as we need to recreate some cache when loading
+-- snapshot, we need to know its type, hence type parameter in the Swapper
+-- would need to carry that information and Swappers differing only in cache
+-- type would not constitute the same type; anyway, this may be changed in the
+-- future.) Cache only keeps references to certain set of values (and is
+-- informed when some value is requested), so keep even weak pointers in
+-- appropriate Swappables alive; when the value drop out of cache (and no
+-- longer referenced from any other place in the program), it is finalized (id
+-- est, saved to the database) and freed.
+--
+-- When all the references to some Swappable (the per-value structure) are lost
+-- and it is about to be garbage collected, it also (in its finalizer) deletes
+-- accompanying entry in the current database.
+--
+--
+-- Snapshots
+--
+-- Because creating snapshot involves writing to external file (and creating
+-- new one), it is not (directly) implement the Serialize class, but Snapshot
+-- instead, which allows running IO actions. When snapshot is about to be
+-- created, all present values are saved to the current database (even those
+-- not currently in memory, because they may be saved in some older database;
+-- for this, the Traversable instance is needed), then we wait for all possibly
+-- running writes to terminate and close then current DB (and reopen it for
+-- further reading), and create new one, with filename determined by given
+-- prefix and current counter.
+--
+-- Loading snapshot is rather straitforward: Accounting data and database
+-- filename are extracted from snapshot, then that database is opened for
+-- reading, actual data are then retrieved, when they are needed. New database
+-- is created, with filename again given by prefix and counter, which is also
+-- loaded from snapshot. Due to this, snapshot of Swapper should be created in
+-- single-threaded manner. Databases assigned to some snapshot always contain
+-- all values of the Swapper on which the putToSnapshot was call; it may
+-- contain also some other values, if there were other Swappers "descended"
+-- from the same mkSwapper call.
+
+
 module Data.Disk.Swapper (
         Swapper,
         mkSwapper,
@@ -35,6 +89,7 @@ module Data.Disk.Swapper (
         getting,
         changing,
 ) where
+
 
 import Control.Concurrent.MVar
 import Control.Concurrent.QSemN
@@ -61,12 +116,17 @@ import qualified Data.Disk.TokyoCabinet as TC
 type Key = ByteString
 
 
+-- Datatype containing information for single value – its key, weak pointer to
+-- actual data and finalization lock (which is emptied when data is created or
+-- loaded and filled when they are saved to disk).
 data Swappable a = Swappable
         { saKey :: !Key
         , saValue :: !(MVar (Weak (a, IO ())))
         , saFinalized :: !(MVar ())
         }
 
+-- The Swapper type; contains database information, counter for generatin key
+-- of newly added items, cache and actual content (consisting of Swappables).
 data Swapper f a = Swapper
         { spDB :: !SwapDB
         , spCounter :: !(MVar Integer)
@@ -74,15 +134,30 @@ data Swapper f a = Swapper
         , spContent :: !(f (Swappable a))
         }
 
+-- Data needed to manage access to database
 data SwapDB = SwapDB
         { sdDB :: !(MVar [TC.Database])
+        -- List of open databases, the first one is for writing, the rest are
+        -- read-only databases, created when loading snapshot (the database
+        -- accompanying the snapshot), or when creating snapshot (the old
+        -- database, which was formerly used for writing).
+
         , sdPrefix :: !FilePath
         , sdCounter :: !(IORef Integer)
+        -- These are to components of database filename, the first is prefix,
+        -- after which is appended current value of the counter (which is then
+        -- incremented) and finelly there goes the extension.
+
         , sdSem :: QSemN
+        -- Semaphore to force synchronisation when changing databases.
         }
 
 
 
+-- Auxiliary functions for accessing the DB
+
+-- They take into account that data can be read from multiple databases and
+-- some locking needed for synchronization when snapshots are created
 
 dbOpen :: FilePath -> IO SwapDB
 dbOpen prefix = do
@@ -115,6 +190,7 @@ dbOut :: SwapDB -> ByteString -> IO Bool
 dbOut db key = withDB db $ \(cdb:_) -> TC.out cdb key
 
 
+-- Instances of Typeable and Snapshot classes
 
 instance (Typeable a) => Typeable (Swappable a) where
         typeOf _ = mkTyConApp (mkTyCon "Data.Disk.Swapper.Swappable")
@@ -228,6 +304,7 @@ swPutNewWeak sa (x, refresh) db = do
         putMVar (saValue sa) value
 
 
+-- Actual loading of data from database
 swLoader :: (Serialize a) => Swapper f a -> Key -> IO (a, IO ())
 swLoader sp key = do
         IO.putStrLn $ "Nacitani " ++ show (fst (deserialize key) :: Integer)
@@ -241,6 +318,8 @@ swLoader sp key = do
              Nothing -> error "Data not found in DB!"
 
 
+-- Takes care about creating Swappables from items newly added to the
+-- structure; their initialization and assigning of finalizers.
 putSwappable :: (Serialize a, NFData a) => Swapper f a -> a -> Swappable a
 putSwappable sp x = deepseq x `pseq` unsafePerformIO $ do
         c <- takeMVar (spCounter sp)
@@ -263,6 +342,8 @@ putSwappable sp x = deepseq x `pseq` unsafePerformIO $ do
         return' swappable
 
 
+-- Retrievs data from swapper, possibly loading it from the database using
+-- swLoader, and calls refresh function on appropriate object in cache.
 getSwappable :: (Serialize a) => Swapper f a -> Swappable a -> a
 getSwappable sp sa = unsafePerformIO $ do
         wx <- takeMVar $ saValue sa
