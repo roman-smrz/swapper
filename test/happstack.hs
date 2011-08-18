759a24b3beb9e238c557ac8cf3c462e86c7823d9
Roman Smrž <roman.smrz@seznam.cz>
2011-08-18 12:44:43 +0200

    Refactoring of the Happstack example

--- test/happstack.hs
+++ test/happstack.hs
@@ -5,7 +5,6 @@ module Main where
 
 import Prelude hiding (lookup)
 
-import Control.Concurrent.MVar
 import Control.DeepSeq
 import Control.Monad
 import Control.Monad.State
@@ -16,19 +15,15 @@ import Data.Typeable
 import Happstack.State
 
 import System.IO.Unsafe
+import System.Mem
 
 import Data.Disk.Cache
 import Data.Disk.Swapper
 import Data.Disk.Swapper.HappstackCompat
 
 
-instance NFData BS.ByteString where
-        rnf x = x `seq` ()
-
-
-
 data AppState = AppState (Swapper [] BS.ByteString)
-        deriving Typeable
+    deriving Typeable
 
 rootState :: Proxy AppState
 rootState = Proxy
@@ -36,43 +31,55 @@ rootState = Proxy
 instance Version AppState
 
 
+-- initial swapper to be used when none was loaded from snapshot
 {-# NOINLINE initialSwapper #-}
-initialSwapper :: MVar (Maybe (Swapper [] BS.ByteString))
-initialSwapper = unsafePerformIO $ newMVar Nothing
-
-addState :: BS.ByteString -> Update AppState ()
-addState x = modify $ \(AppState s) -> AppState $ adding (:) x s
-
-$(mkMethods ''AppState ['addState])
-
+initialSwapper :: Swapper [] BS.ByteString
+initialSwapper = unsafePerformIO $ do
+    cache <- mkClockCache 5
+    mkSwapper "_local/happstack_state/data" cache []
 
 instance Component AppState where
-        type Dependencies AppState = End
-        initialValue = unsafePerformIO $ do
-                mbsw <- takeMVar initialSwapper
-                case mbsw of Just sw -> do
-                                     putMVar initialSwapper mbsw
-                                     return $ AppState sw
-                             Nothing -> do
-                                     cache <- mkClockCache 5
-                                     sw <- mkSwapper "_local/happstack_state/data" cache []
-                                     putMVar initialSwapper (Just sw)
-                                     return $ AppState sw
-
+    type Dependencies AppState = End
+    initialValue = AppState initialSwapper
 
 instance Serialize AppState where
-        getCopy = contain $ fmap AppState safeGet
-        putCopy (AppState s) = contain $ safePut s
+    getCopy = contain $ fmap AppState safeGet
+    putCopy (AppState s) = contain $ safePut s
+
+
+-- instance needed for Swapper
+instance NFData BS.ByteString where
+    rnf x = x `seq` ()
+
+
+
+
+-- adds entry to the swapper in global state
+addState :: BS.ByteString -> Update AppState ()
+addState x = do
+    AppState s <- get
+    let s' = adding (:) x s
+    put $ AppState s'
+
+    -- evaluate the Swapper object to HNF in order to allow the data to be
+    -- swapped-out (instead of being held by unevaluated thunks)
+    s' `seq` return ()
+
+
+$(mkMethods ''AppState ['addState])
 
 
 
 main :: IO ()
 main = do
-        control <- startSystemState rootState
+    control <- startSystemState rootState
 
-        forM_ [1..5] $ \_ -> do
-                d <- BS.readFile "in"
-                update $ AddState d
+    forM_ [1..10] $ \_ -> do
+        d <- BS.readFile "in"
+        update $ AddState d
+        performGC
 
-        createSwapperCheckpoint control
-        shutdownSystem control
+    -- we need to call this wrapper around createCheckpoint
+    createSwapperCheckpoint control
+
+    shutdownSystem control
